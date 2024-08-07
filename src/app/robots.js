df418b5da235bf3c67f0234d6743c9eb42e7c06a
nangdosan <nangdosanprojects@gmail.com>
2024-08-07 18:08:04 +0700

    delete
    sitemap and robots

--- src/app/robots.js
+++
@@ -1,6 +0,0 @@
-export default function robots() {
-  return {
-    rules: [{ userAgent: "*", allow: "/", disallow: ["/dashboard/"] }],
-    sitemap: "https://sadawira-yasa-utama.vercel.app/sitemap.xml",
-  };
-}
